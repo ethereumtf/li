3d0fa8148b24120e241765c45596074376c29c4f
Benjamin <myethereumtf@gmail.com>
2024-03-16 14:47:54 +0000

    Update SmartContract

--- Backend/packages/hardhat/contracts/DiceGame.sol
+++ Backend/packages/hardhat/contracts/DiceGame.sol
@@ -23,17 +23,36 @@ contract DiceGame {
         require(msg.value >= 0.002 ether, "Failed to send enough value");
 
         bytes32 prevHash = blockhash(block.number - 1);
-        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce));
-        uint256 roll = uint256(hash) % 16;
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce++));
+        uint256 roll = uint256(hash) % 10;
+
+        bytes32 prevHash1 = blockhash(block.number - 1);
+        bytes32 hash1 = keccak256(abi.encodePacked(prevHash1, address(this), nonce++));
+        uint256 roll1 = uint256(hash1) % 10;
+
+        bytes32 prevHash2 = blockhash(block.number - 1);
+        bytes32 hash2 = keccak256(abi.encodePacked(prevHash2, address(this), nonce++));
+        uint256 roll2 = uint256(hash2) % 10;
+
+        bytes32 prevHash3 = blockhash(block.number - 1);
+        bytes32 hash3 = keccak256(abi.encodePacked(prevHash3, address(this), nonce++));
+        uint256 roll3 = uint256(hash3) % 10;
+
 
         console.log('\t',"   Dice Game Roll:",roll);
+        console.log('\t',"   Dice Game Roll:",roll1);
+        console.log('\t',"   Dice Game Roll:",roll2);
+        console.log('\t',"   Dice Game Roll:",roll3);
 
         nonce++;
         prize += ((msg.value * 40) / 100);
 
         emit Roll(msg.sender, roll);
+        emit Roll(msg.sender, roll1);
+        emit Roll(msg.sender, roll2);
+        emit Roll(msg.sender, roll3);
 
-        if (roll > 2 ) {
+        if (roll % 2 != 0) {
             return;
         }
 
